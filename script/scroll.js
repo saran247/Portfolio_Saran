3520b902ca9e3e49d0d17b91eca5bc91f18d2859
saran247 <kirithiksaran@gmail.com>
2024-09-04 20:30:22 +0530

    new commit

--- script/scroll.js
+++ script/scroll.js
@@ -1,10 +1,10 @@
-ScrollReveal({
-    distance: '80px',
-    duration: 2000,
-    delay: 100,
-});
+// ScrollReveal({
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 100,
+// });
 
-// ScrollReveal().reveal('', { origin: 'top' });
-ScrollReveal().reveal('.card', { origin: 'bottom' });
-ScrollReveal().reveal('.obj_desc', { origin: 'left' });
-ScrollReveal().reveal('.con_img,.desc,hr', { origin: 'right' });
+// // ScrollReveal().reveal('', { origin: 'top' });
+// ScrollReveal().reveal('.card', { origin: 'bottom' });
+// ScrollReveal().reveal('.obj_desc', { origin: 'left' });
+// ScrollReveal().reveal('.con_img,.desc,hr', { origin: 'right' });
